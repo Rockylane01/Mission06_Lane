8c3becd9ff77af75b126af6b4c4b3524cd937f7f
Rockylane01 <rockylane01@gmail.com>
2026-02-09 09:03:18 -0700

    Add project files.

---
+++ Mission06_Lane/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission06_Lane.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
