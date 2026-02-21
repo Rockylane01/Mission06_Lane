1934576a9b0092089be9ac082bed57842dad3108
Rockylane01 <rockylane01@gmail.com>
2026-02-20 18:23:21 -0700

    Add validation for backend and the delete action

--- Mission06_Lane/Controllers/HomeController.cs
+++ Mission06_Lane/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace Mission06_Lane.Controllers
             return View();
         }
 
+        // GET for Movie List
+        [HttpGet]
         public IActionResult MovieList()
         {
             var Movies = _context.Movies.ToList();
@@ -34,15 +36,66 @@ namespace Mission06_Lane.Controllers
         [HttpGet]
         public IActionResult AddMovie()
         {
-            return View();
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View(new Movie());
         }
 
         [HttpPost]
         public IActionResult AddMovie(Movie m)
         {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = _context.Categories.ToList();
+                return View(m);
+            }
             _context.Add(m);
             _context.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("MovieList");
+        }
+
+        // GET and POST for Edit Movie
+        [HttpGet]
+        public IActionResult EditMovie(int id)
+        {
+            var m = _context.Movies
+                .Single(m => m.MovieId == id);
+
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View("AddMovie", m);
+        }
+
+        [HttpPost]
+        public IActionResult EditMovie(Movie m)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = _context.Categories.ToList();
+                return View("AddMovie", m);
+            }
+
+            _context.Update(m);
+            _context.SaveChanges();
+            return RedirectToAction("MovieList");
+        }
+
+        // GET and POST for Delete Movie
+        [HttpGet]
+        public IActionResult DeleteMovie(int id)
+        {
+            var record = _context.Movies.Single(x => x.MovieId == id);
+
+            return View(record);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteMovie(Movie m)
+        {
+            _context.Movies.Remove(m);
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieList");
         }
     }
 }
--- Mission06_Lane/Models/Movie.cs
+++ Mission06_Lane/Models/Movie.cs
@@ -1,19 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mission06_Lane.Models
 {
     public class Movie
     {
         [Key]
-        public required int MovieID { get; set; }
+        [Required]
+        public int MovieId { get; set; }
+
+        [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
-        public required string Title { get; set; }
-        public required int Year { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Year is required")]
+        [Range(1888, 9999, ErrorMessage = "Year must be between 1888 and 2100")]
+        public int? Year { get; set; }
+
         public string? Director { get; set; }
+
         public string? Rating { get; set; }
-        public required int Edited { get; set; }
+
+        [Required(ErrorMessage = "Edited status is required")]
+        public int? Edited { get; set; }
+
         public string? LentTo { get; set; }
-        public required int CopiedToPlex { get; set; }
+
+        [Required(ErrorMessage = "Copied to Plex status is required")]
+        public int? CopiedToPlex { get; set; }
+
         public string? Notes { get; set; }
     }
 }
